638c38dd0d0eed5a8de7cc7c7ec61cd86c2b20c9
Bato Balint <balint.bato@gmail.com>
2021-09-25 17:39:29 +0200

    init kesz

--- app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.clickcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView : TextView
+    lateinit var buttonUp : Button
+    lateinit var buttonDown : Button
+    var szam = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Init()
+
+        buttonUp.setOnClickListener {
+
+        }
+    }
+
+    fun Init() {
+        textView = findViewById(R.id.tv_szam)
+        textView.setText(szam.toString())
+        buttonUp = findViewById(R.id.button_up)
+        buttonDown = findViewById(R.id.button_down)
     }
 }
